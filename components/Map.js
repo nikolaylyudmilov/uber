85214266acd65334203e6866622071ae06835486
Nikolay <nlyudmilov.bor@gmail.com>
2022-11-05 00:21:13 +0200

    Ride Options added

--- components/Map.js
+++ components/Map.js
@@ -1,71 +1,63 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useRef } from 'react'
-import MapView, { Marker } from "react-native-maps"
+import MapView, { Marker } from 'react-native-maps'
+import { useSelector } from 'react-redux'
+import { selectDestination, selectOrigin } from '../slices/navSlice'
+import MapViewDirections from 'react-native-maps-directions'
+import { GOOGLE_MAPS_APIKEY } from "@env"
 import tw from 'tailwind-react-native-classnames'
-import { useSelector } from "react-redux"
-import { selectDestination, selectOrigin } from "../slices/navSlice"
-import MapViewDirections from "react-native-maps-directions"
-import { GOOGLE_MAPS_APIKEY } from '@env'
 
 
 const Map = () => {
-  const origin = useSelector(selectOrigin);
-  const destination = useSelector(selectDestination)
-  const mapRef = useRef(null);
+    const origin = useSelector(selectOrigin)
+    const destination = useSelector(selectDestination);
+    const mapRef = useRef(null);
 
-  useEffect(()=> {
-    if (!origin || !destination) return;
+    useEffect(() => {
+      if (!origin || !destination) return;
 
-    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
-      edgePadding: {top:50, right:50, bottom:50, left:50}
-    })
-  }, [origin, destination])
+
+      mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+        edgePadding: {top: 50, right: 50, bottom: 50, left: 50}
+      })
+    }, [origin, destination]);
+
   return (
     <MapView
-    ref={mapRef}
-    style = {tw `flex-1`}
-    mapType="mutedStandard"
-    initialRegion = {{
+      ref={mapRef}
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
       latitude: origin.location.lat,
       longitude: origin.location.lng,
       latitudeDelta: 0.005,
       longitudeDelta: 0.005,
-    }}>
-
+    }}
+    >
       {origin && destination && (
-        <MapViewDirections 
-          origin={origin.description}
-          destination={destination.description}
-          apikey={GOOGLE_MAPS_APIKEY}
-          strokeWidth={3}
-          strokeColor="black"
-        />
-      )}
-
+      <MapViewDirections
+       origin={origin.description}
+       destination={destination.description}
+       apikey={GOOGLE_MAPS_APIKEY}
+       strokeWidth={3}
+       strokeColor="black" />)}
       {origin?.location && (
-        <Marker
-          coordinate={{
-            latitude: origin.location.lat,
-            longitude: origin.location.lng,
-          }}
-          title="Origin"
-          description={origin.description}
-          indentifier="origin"
-        />
+          <Marker coordinate={{
+              latitude: origin.location.lat,
+              longitude: origin.location.lng}}
+              title="Origin"
+              description={origin.description}
+              identifier="origin"/>
       )}
-
       {destination?.location && (
-        <Marker
-          coordinate={{
-            latitude: destination.location.lat,
-            longitude: destination.location.lng,
-          }}
-          title="Destination"
-          description={destination.description}
-          indentifier="destination"
-        />
+          <Marker coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng}}
+              title="Destination"
+              description={destination.description}
+              identifier="destination"/>
       )}
-    </MapView>
+   </ MapView>
   )
 }
 
