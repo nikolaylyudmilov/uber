0ebb786559626b48aa7c2f887e46175a83222d15
Nikolay <nlyudmilov.bor@gmail.com>
2022-11-04 15:02:08 +0200

    Added RideOptionsCard

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,8 @@ import React from 'react'
 import tw from "tailwind-react-native-classnames";
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const data = [{
         id: "123",
@@ -20,7 +22,7 @@ const data = [{
 
 const NavOptions = () => {
     const navigation = useNavigation();
-
+    const origin = useSelector(selectOrigin);
 
     return (
       <FlatList 
@@ -31,8 +33,9 @@ const NavOptions = () => {
           <TouchableOpacity
             onPress={() => navigation.navigate(item.screen)}
             style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+            disabled={!origin}
           >
-              <View>
+              <View style={tw`${!origin && "opacity-20"}`}>
                   <Image
                   style={{width: 120, height: 102, resizeMode: "contain"}}
                    source={{uri: item.image}}/>
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import Map from '../components/Map'
+import MapView from 'react-native-maps';
+import NavigateCard from '../components/NavigateCard';
+import { createStackNavigator } from '@react-navigation/stack';
+import RideOptions from '../components/RideOptions';
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
+
   return (
     <View>
       <Text>Here is the map staff...</Text>
+
+      <View style={tw `h-1/2`}>
+        <Map />
+      </View>
+
+      <View style={tw `h-1/2`}>
+        <Stack.Navigator>
+            <Stack.Screen 
+              name="NavigateCard"
+              component={NavigateCard}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name="RideOptions"
+              component={RideOptions}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+      </View>
     </View>
   )
 }
